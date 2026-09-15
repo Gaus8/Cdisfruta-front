91e6db943047449c431e1d7632142c1be3fe4ae5
GordiGT <efgordillo@ucundinamarca.edu.co>
2026-09-15 12:34:48 -0500

    feat: importacion de mis pedidos en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import MainPage from './paginas/landingPage/MainPage';
 import Validacion from "./paginas/usuariosAuth/Validacion";
 import DashboardUsuario from "./paginas/paginaClientes/DashboardUsuario";
 import ConfiguracionUsuario from "./paginas/paginaClientes/ConfiguracionUsuario";
+import MisPedidos from "./paginas/paginaClientes/MisPedidos"; 
 import DashboardAdmin from "./paginas/dashboardAdmin/DashboardAdmin";
 import Productos from "./paginas/dashboardAdmin/productos/Productos";
 import HomeAdmin from "./paginas/dashboardAdmin/HomeAdmin";
